d82b2f005db3ed653583751b05a1e9a81dae005d
DK <daekyeongp96@gmail.com>
2021-11-02 14:13:15 +0900

    데이터 정제 (1) missing value.md commit

--- git-load.py
+++ git-load.py
@@ -16,17 +16,17 @@ print("file_list: {}".format(file_list_py))
 # 현재 시간 기준 날짜
 time2 = datetime.now()
 count = 0  # 횟수 count
-print((time2 - timedelta(days=159)).strftime('%a %b %d %H:%M:%S %Y'))
+print((time2 - timedelta(days=156)).strftime('%a %b %d %H:%M:%S %Y'))
 # exit(0)
 for i in range(len(file_list_py)):
 # for i in range(5):
     # 날짜 변수
-    what_day = (time2 - timedelta(days=159-i)).strftime('%a %b %d %H:%M:%S %Y')
+    what_day = (time2 - timedelta(days=156-i)).strftime('%a %b %d %H:%M:%S %Y')
     count += 1
     # print(what_day)
     # 파일 로딩
     with open(path+file_list_py[i], 'r', encoding="utf-8") as f:
-        md = open("C:\\github\\data-handle-with-python\\numpy\\" + file_list_py[i], 'w', encoding='utf-8')
+        md = open("C:\\github\\data-handle-with-python\\Data Cleaning\\" + file_list_py[i], 'w', encoding='utf-8')
         text = f.read()
         md.write(f'{text}')
         f.close()
@@ -41,3 +41,4 @@ for i in range(len(file_list_py)):
     os.system(f'git push origin +master')
 
     print(f'{i + 1}회차 완료되었음')
+    print(count)
